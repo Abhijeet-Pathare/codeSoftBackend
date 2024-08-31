91e2485c6c47c28034c75f19a818a3305436700f
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2024-08-31 10:58:54 +0530

    added changes to Course and UserController

--- src/main/java/com/codesoft/app/service/UserService.java
+++ src/main/java/com/codesoft/app/service/UserService.java
@@ -1,5 +1,7 @@
 package com.codesoft.app.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +22,9 @@ public class UserService {
 
 	public User findByUsername(String username) {
 		return userRepository.findByUsername(username);
+	}
+
+	public List<User> findAll() {
+		return userRepository.findAll();
 	}
 }
